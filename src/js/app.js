86542b883a14fbc92dd722c878e74d967571c631
Pawel <pablomiklas@gmail.com>
2018-11-22 20:23:38 +0100

    Add slider and remove dist folder

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,60 @@
 // import "bootstrap";
 // require("../style/main.scss ");
-const listButton = document.querySelectorAll(".list__button");
-const sectionContent = document.querySelectorAll(".section__content");
-const nameOfContent = [
-    "transform-to-home",
-    "transform-to-project",
-    "transform-to-about"
-];
-let x = false;
-
-function scrollToSection(index) {
-    sectionContent.forEach((section) => {
-        section.className = "section__content";
-        section.classList.add(nameOfContent[index]);
-    });
-}
-
-listButton.forEach((list, index) => {
-    list.addEventListener("click", () => {
-        listButton.forEach((btn) =>
-            btn.classList.remove("list__button--active")
-        );
-        // list.classList.remove("list__button--active");
-        scrollToSection(index);
-        list.classList.add("list__button--active");
-    });
-});
-document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
-        document.querySelector("#loader-wrapper").classList.add("out");
-    }
-};
-
 // setTimeout(() => {
 //     document.querySelector("#loader-wrapper").classList.add("out");
-// }, 5000);
+// }, 1000);
+const preLoader = (() => {
+    window.addEventListener("load", () => {
+        document.querySelector("#loader-wrapper").classList.add("out");
+    });
+})();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const slider = (() => {
+        const arrowRight = document.querySelector(".fa-angle-right");
+        const arrowLeft = document.querySelector(".fa-angle-left");
+        const first = document.querySelector("#first-slide");
+        const second = document.querySelector("#second-slide");
+        const third = document.querySelector("#third-slide");
+        const fourth = document.querySelector("#fourth-slide");
+        let x = 130;
+        arrowRight.addEventListener("click", () => {
+            if (x > 390) return;
+            first.style.webkitTransform = `translateX(-${x}%)`;
+            second.style.webkitTransform = `translateX(-${x - 130}%)`;
+            third.style.webkitTransform = `translateX(-${x - 260}%)`;
+            fourth.style.webkitTransform = `translateX(-${x - 390}%)`;
+            x += 130;
+        });
+        arrowLeft.addEventListener("click", () => {
+            if (x === 130) return;
+            x -= 130;
+            first.style.webkitTransform = `translateX(${-Math.abs(x) + 130}%)`;
+            second.style.webkitTransform = `translateX(${-Math.abs(x) + 260}%)`;
+            third.style.webkitTransform = `translateX(${-Math.abs(x) + 390}%)`;
+            fourth.style.webkitTransform = `translateX(${-Math.abs(x) + 520}%)`;
+        });
+    })();
+
+    const slideVertical = (() => {
+        const listButton = document.querySelectorAll(".list__button");
+        const sectionContent = document.querySelectorAll(".section__content");
+        const nameOfContent = [
+            "transform-to-home",
+            "transform-to-project",
+            "transform-to-about"
+        ];
+        listButton.forEach((list, index) => {
+            list.addEventListener("click", () => {
+                listButton.forEach((btn) =>
+                    btn.classList.remove("list__button--active")
+                );
+                sectionContent.forEach((section) => {
+                    section.className = "section__content";
+                    section.classList.add(nameOfContent[index]);
+                });
+                list.classList.add("list__button--active");
+            });
+        });
+    })();
+});
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,55 +1,61 @@
-const glob = require('glob');
-const path = require('path');
+const glob = require("glob");
+const path = require("path");
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const generateHTMLPlugins = () =>
-	glob.sync('./src/**/*.html').map(
-		(dir) =>
-			new HTMLWebpackPlugin({
-				filename: path.basename(dir), // Output
-				template: dir // Input
-			})
-	);
+    glob.sync("./src/**/*.html").map(
+        (dir) =>
+            new HTMLWebpackPlugin({
+                filename: path.basename(dir), // Output
+                template: dir, // Input
+                inject: "head"
+            })
+    );
 
 module.exports = {
-	node: {
-		fs: 'empty'
-	},
-	entry: [ './src/js/app.js', './src/style/main.scss' ],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'app.bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.(sass|scss)$/,
-				use: [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ]
-			},
-			{
-				test: /\.html$/,
-				loader: 'raw-loader'
-			}
-		]
-	},
-	plugins: [
-		new CopyWebpackPlugin([
-			{
-				from: './src/static/',
-				to: './static/'
-			}
-		]),
-		...generateHTMLPlugins()
-	],
-	stats: {
-		colors: true
-	},
-	devtool: 'source-map'
+    node: {
+        fs: "empty"
+    },
+    entry: ["./src/js/app.js", "./src/style/main.scss"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "app.bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: "raw-loader"
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: "./src/static/",
+                to: "./static/"
+            }
+        ]),
+        ...generateHTMLPlugins()
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: "source-map"
 };
