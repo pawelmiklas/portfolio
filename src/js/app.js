9296b686dd92ec26d75d051a5a98613d91000a1b
Pawel <pablomiklas@gmail.com>
2018-11-22 23:00:17 +0100

    Modify js script with some foreaches

--- src/js/app.js
+++ src/js/app.js
@@ -4,57 +4,52 @@
 //     document.querySelector("#loader-wrapper").classList.add("out");
 // }, 1000);
 const preLoader = (() => {
-    window.addEventListener("load", () => {
-        document.querySelector("#loader-wrapper").classList.add("out");
-    });
+	window.addEventListener('load', () => {
+		document.querySelector('#loader-wrapper').classList.add('out');
+	});
 })();
 
-document.addEventListener("DOMContentLoaded", () => {
-    const slider = (() => {
-        const arrowRight = document.querySelector(".fa-angle-right");
-        const arrowLeft = document.querySelector(".fa-angle-left");
-        const first = document.querySelector("#first-slide");
-        const second = document.querySelector("#second-slide");
-        const third = document.querySelector("#third-slide");
-        const fourth = document.querySelector("#fourth-slide");
-        let x = 130;
-        arrowRight.addEventListener("click", () => {
-            if (x > 390) return;
-            first.style.webkitTransform = `translateX(-${x}%)`;
-            second.style.webkitTransform = `translateX(-${x - 130}%)`;
-            third.style.webkitTransform = `translateX(-${x - 260}%)`;
-            fourth.style.webkitTransform = `translateX(-${x - 390}%)`;
-            x += 130;
-        });
-        arrowLeft.addEventListener("click", () => {
-            if (x === 130) return;
-            x -= 130;
-            first.style.webkitTransform = `translateX(${-Math.abs(x) + 130}%)`;
-            second.style.webkitTransform = `translateX(${-Math.abs(x) + 260}%)`;
-            third.style.webkitTransform = `translateX(${-Math.abs(x) + 390}%)`;
-            fourth.style.webkitTransform = `translateX(${-Math.abs(x) + 520}%)`;
-        });
-    })();
+document.addEventListener('DOMContentLoaded', () => {
+	const slider = (() => {
+		const arrowRight = document.querySelector('.fa-angle-right');
+		const arrowLeft = document.querySelector('.fa-angle-left');
+		const sectionSlides = document.querySelectorAll('.section__slide');
+		let rotateValue = 130;
+		let valueOfRotate = 0;
+		let valueOfRotate2 = 130;
+		arrowRight.addEventListener('click', () => {
+			if (rotateValue > 390) return;
+			sectionSlides.forEach((slide) => {
+				slide.style.webkitTransform = `translateX(-${rotateValue - valueOfRotate}%)`;
+				valueOfRotate += 130;
+			});
+			rotateValue += 130;
+			valueOfRotate = 0;
+		});
+		arrowLeft.addEventListener('click', () => {
+			if (rotateValue === 130) return;
+			rotateValue -= 130;
+			sectionSlides.forEach((slide) => {
+				slide.style.webkitTransform = `translateX(${-Math.abs(rotateValue) + valueOfRotate2}%)`;
+				valueOfRotate2 += 130;
+			});
+			valueOfRotate2 = 130;
+		});
+	})();
 
-    const slideVertical = (() => {
-        const listButton = document.querySelectorAll(".list__button");
-        const sectionContent = document.querySelectorAll(".section__content");
-        const nameOfContent = [
-            "transform-to-home",
-            "transform-to-project",
-            "transform-to-about"
-        ];
-        listButton.forEach((list, index) => {
-            list.addEventListener("click", () => {
-                listButton.forEach((btn) =>
-                    btn.classList.remove("list__button--active")
-                );
-                sectionContent.forEach((section) => {
-                    section.className = "section__content";
-                    section.classList.add(nameOfContent[index]);
-                });
-                list.classList.add("list__button--active");
-            });
-        });
-    })();
+	const slideVertical = (() => {
+		const listButton = document.querySelectorAll('.list__button');
+		const sectionContent = document.querySelectorAll('.section__content');
+		const nameOfContent = ['transform-to-home', 'transform-to-project', 'transform-to-about'];
+		listButton.forEach((list, index) => {
+			list.addEventListener('click', () => {
+				listButton.forEach((btn) => btn.classList.remove('list__button--active'));
+				sectionContent.forEach((section) => {
+					section.className = 'section__content';
+					section.classList.add(nameOfContent[index]);
+				});
+				list.classList.add('list__button--active');
+			});
+		});
+	})();
 });
