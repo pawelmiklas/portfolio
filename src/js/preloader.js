84b4f0a4e14e0efd759ecd9f54fa56b469903ee7
pawelmiklas <pablomiklas@gmail.com>
2018-11-28 23:35:15 +0100

    small add

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,4 +1,6 @@
 console.log('preoloader');
+console.log('preoloader');
+console.log('preoloader');
 const preLoader = (() => {
 	window.addEventListener('load', () => {
 		document.querySelector('#loader-wrapper').classList.add('out');
