ca5e0c0c0ad3ffd31e05c33cb4e51599f4fd1ee8
pawelmiklas <pablomiklas@gmail.com>
2019-03-03 17:31:34 +0100

    today commit

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,9 @@
 const merge = require('webpack-merge');
-
 const common = require('./webpack.config.common.js');
-
 module.exports = merge(common, {
-  mode: 'development',
-  devServer: {
-    open: true,
-    port: 9000,
-  },
+	mode: 'development',
+	devServer: {
+		open: true,
+		port: 9000,
+	},
 });
